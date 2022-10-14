f677d9c9f9419b96ffc3d6a3807afd726e22cf1e
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-10-14 14:20:54 +0000

    refactor complete, new background

--- app/src/OneComponentToRuleThemAll.jsx
+++ app/src/OneComponentToRuleThemAll.jsx
@@ -1,24 +1,24 @@
-export function AppetizerButton(props) {
+export function Button(props) {
     
-    let selections = (props.menu.filter((item) => (item.category.title === 'Appetizer')))
+    let selections = (props.menu.filter((item) => (item.category.title === props.search)))
     
     let items = [];
 
     for(let i = 0; i < selections.length; i++) {
         items.push( <>
-                        <div key={i} className="col-lg-3 col-sm-6" id={"accordion" + i}>
+                        <div key={i} className="col-lg-3 col-sm-6 menu-display" id={"accordion" + i}>
                             <div className="card">
                               <div className="card-header" id={"heading" + i}>
                                 <h4>Le {selections[i].title}</h4>
                                 <h6>${selections[i].price}</h6>
                                 <p>
-                                    <a className="btn btn-light" data-bs-toggle="collapse" href={"#appDesc" + i} role="button" aria-expanded="false" aria-controls={"appDesc" + i}>
-                                DESCRIPTION
+                                    <a className="btn btn-light" data-bs-toggle="collapse" href={"#" + props.search + "Desc" + i} role="button" aria-expanded="false" aria-controls={props.search + "Desc" + i}>
+                                Description
                                 </a>
                                 </p>
                                 <div className="row">
                                     <div className="col">
-                                      <div className="collapse multi-collapse" id={"appDesc" + i}>
+                                      <div className="collapse multi-collapse" id={props.search + "Desc" + i}>
                                         <div className="card card-body">
                                           {selections[i].description}
                                         </div>
@@ -29,5 +29,5 @@ export function AppetizerButton(props) {
                             </div>
                         </div>
                     </>)
-    } return(<div className="container-fluid"><div className="container-fluid"><div className="row">{items}</div></div></div>)
+    } return(<div className="container-fluid menu-background"><div className="row"><h1>{props.search}</h1>{items}</div></div>)
 }
