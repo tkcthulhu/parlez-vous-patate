ca893384356025f97ea023ff846a0bd8d293168e
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-10-16 21:22:40 +0000

    more formatting

--- app/src/OneComponentToRuleThemAll.jsx
+++ app/src/OneComponentToRuleThemAll.jsx
@@ -5,30 +5,46 @@ export function Button(props) {
     let items = [];
 
     for(let i = 0; i < selections.length; i++) {
-        items.push( <>
-                        <div key={i} className="col-lg-3 col-sm-6 menu-display" id={"accordion" + i}>
-                            <div className="card">
-                              <div className="card-header" id={"heading" + i}>
-                                <h4>Le {selections[i].title}</h4>
-                                <h6>${selections[i].price}</h6>
-                                <p>
-                                  <a className="btn btn-light" data-bs-toggle="collapse" href={"#" + props.search + "Desc" + i} role="button" aria-expanded="false" aria-controls={props.search + "Desc" + i}>
-                                    Description
-                                  </a>                                 
-                                </p>
-                                <div className="row">
-                                    <div className="col">
-                                      <div className="collapse multi-collapse" id={props.search + "Desc" + i}>
-                                        <div className="card card-body">
-                                          {selections[i].description}
-                                        </div>
-                                      </div>
-                                    </div>
-                                </div>
-                                <button className="btn btn-light" onClick={() => {props.setCART([selections[i], ...props.CART])}}>Add to Basket</button>
-                              </div>
-                            </div>
+      items.push( 
+        <>
+          <div key={i} className="col-lg-3 col-sm-6 menu-display" id={"accordion" + i}>
+              <div className="card">
+                <div className="card-header" id={"heading" + i}>
+                  <h4>Le {selections[i].title}</h4>
+                  <h6>${selections[i].price}</h6>
+                  <p>
+                    <a 
+                      className="btn btn-light" 
+                      data-bs-toggle="collapse" 
+                      href={"#" + props.search + "Desc" + i} 
+                      role="button" aria-expanded="false" 
+                      aria-controls={props.search + "Desc" + i}>
+                      Description
+                    </a>                                 
+                  </p>
+                  <div className="row">
+                      <div className="col">
+                        <div className="collapse multi-collapse" id={props.search + "Desc" + i}>
+                          <div className="card card-body">
+                            {selections[i].description}
+                          </div>
                         </div>
-                    </>)
-    } return(<div className="container-fluid menu-background"><div className="row"><h1>{props.search}</h1>{items}</div></div>)
+                      </div>
+                  </div>
+                  <button className="btn btn-light" onClick={() => {props.setCART([selections[i], ...props.CART])}}>
+                    Add to Basket
+                  </button>
+                </div>
+              </div>
+          </div>
+        </>
+      )
+    } 
+    return(
+    <div className="container-fluid menu-background">
+      <div className="row">
+        <h1>{props.search}</h1>{items}
+      </div>
+    </div>
+    )
 }
